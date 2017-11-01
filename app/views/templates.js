ac08ce4401e04105cb7cb1824ba18700dac617da
seifer8ff <brady.bcmp@gmail.com>
2017-11-01 12:18:17 -0700

    More z-index text-shadow fixes
    Fixed an old css selector overlapping with a new one.
    Added opacity hack to fix text-shadow rendering on mobile safari.

--- app/views/templates.js
+++ app/views/templates.js
@@ -127,7 +127,7 @@ Handlebars.registerPartial("heroSummary", Handlebars.template({"1":function(cont
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
     + "\"></div>\n			<div class=\"hero-icon ohi-"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
-    + "\"></div>\n			<h3 class=\"hero-name\">"
+    + "\"></div>\n			<h3 class=\"title\">"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.name : stack1), depth0))
     + "</h3>\n		</div>\n		<hr class=\"tight\">\n		<div class=\"body\">\n"
     + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.stats : stack1)) != null ? stack1.playtime : stack1)) != null ? stack1.value : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
@@ -184,9 +184,9 @@ this["OW"]["templates"]["heroSingle"] = Handlebars.template({"1":function(contai
 
   return "<div id=\"content\">\n	<div class=\"hero-icon full-page ohi-"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
-    + "\"></div>\n	<div class=\"container\">\n		<div class=\"row\">\n			<div class=\"col-xs-12 col-sm-8 col-md-7\">\n				<div class=\"hero single panel\">\n					<div class=\"title-container full\">\n						<div class=\"overlay fill "
+    + "\"></div>\n	<div class=\"container\">\n		<div class=\"row\">\n			<div class=\"col-xs-12 col-sm-8 col-md-7\">\n				<div class=\"hero single panel\">\n					<div class=\"title-container fill\">\n						<div class=\"overlay fill "
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
-    + "\"></div>\n						<h1 class=\"title\">"
+    + "\"></div>\n						<h1 class=\"title fill\">"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.name : stack1), depth0))
     + "</h1>\n					</div>\n					<hr class=\"tight\">\n					<div class=\"body\">\n						<dl class=\"dl-horizontal\">\n							<dt>Real Name</dt>\n							<dd>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.data : stack1)) != null ? stack1.realName : stack1), depth0))
@@ -201,9 +201,9 @@ this["OW"]["templates"]["heroSingle"] = Handlebars.template({"1":function(contai
     + ((stack1 = helpers["if"].call(alias3,((stack1 = ((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.data : stack1)) != null ? stack1.shield : stack1),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "						<hr>\n						<img class=\"img-rounded img-tall\" src=\"../img/ow-"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
-    + "-full.jpg\">\n					</div>\n				</div>\n			</div>\n			<div class=\"col-xs-12 col-sm-4 col-md-4 col-md-offset-1\">\n				<div class=\"panel\">\n					<div class=\"title-container full\">\n						<div class=\"overlay fill "
+    + "-full.jpg\">\n					</div>\n				</div>\n			</div>\n			<div class=\"col-xs-12 col-sm-4 col-md-4 col-md-offset-1\">\n				<div class=\"panel\">\n					<div class=\"title-container fill\">\n						<div class=\"overlay fill "
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.normalizedName : stack1), depth0))
-    + "\"></div>\n						<h2 class=\"title\">Stats</h2>\n					</div>\n					<hr class=\"tight\">\n					<div class=\"body\">\n"
+    + "\"></div>\n						<h2 class=\"title fill\">Stats</h2>\n					</div>\n					<hr class=\"tight\">\n					<div class=\"body\">\n"
     + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.noUser : depth0),{"name":"if","hash":{},"fn":container.program(11, data, 0),"inverse":container.program(13, data, 0),"data":data})) != null ? stack1 : "")
     + "					</div>\n				</div>\n			</div>\n		</div>\n	</div>\n</div>";
 },"usePartial":true,"useData":true});
